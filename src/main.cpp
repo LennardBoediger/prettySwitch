e755c1cb46e302c13d0aac7df79f32a45b9444fc
lennard.boediger <lennard@nitrokey.com>
2025-08-24 23:21:29 +0200

    connection test to home bride set wifi manually in serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include "ElroRemote.h"
+#include "HomeSpan.h"   
+#include "LED.h"
+#include "DEV_RELAY.h"
 
 // Defines for readability
 #define ON 1
@@ -8,13 +11,29 @@
 
 void setup() {
   init();
+  Serial.begin(9600);
+
+  homeSpan.setPairingCode("11122333");
+  homeSpan.setQRID("111-22-333");
+
+  homeSpan.begin(Category::Bridges, "HomeSpan Bridge");
+       
+
+  new SpanAccessory();
+    new Service::AccessoryInformation();
+      new Characteristic::Identify();
+    new LED(15); 
+
+   
+  // Accessory 2: Relay Switch
+  new SpanAccessory();
+    new Service::AccessoryInformation();    
+      new Characteristic::Identify();                       
+    new DEV_RELAY(17);  // instantiates a new relay
 }
 
 void loop() {
   
-  remote(1, A_SWITCH, ON);
-  delay(3000);
-  remote(1, A_SWITCH, OFF);
-  delay(3000);
+  homeSpan.poll();
 
 }
