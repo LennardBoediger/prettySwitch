2c1ec1cdc0a27cbb63cd7620209c67465fcc8254
lennard.boediger <lennard@nitrokey.com>
2025-08-25 00:26:21 +0200

    homespan hello world - control remote(1, A_SWITCH, OFF);

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include "ElroRemote.h"
-#include "HomeSpan.h"   
-#include "LED.h"
-#include "DEV_RELAY.h"
+#include "HomeSpan.h"
+#include "DEV_LED.h"
 
 // Defines for readability
 #define ON 1
 #define OFF 0
+
 
 
 void setup() {
@@ -16,24 +16,42 @@ void setup() {
   homeSpan.setPairingCode("11122333");
   homeSpan.setQRID("111-22-333");
 
-  homeSpan.begin(Category::Bridges, "HomeSpan Bridge");
-       
+  homeSpan.begin(Category::Lighting,"HomeSpan LED");
+  
+  new SpanAccessory(); 
+  
+    new Service::AccessoryInformation(); 
+      new Characteristic::Identify();  //  In Example 2 we instantiated a LightBulb Service and its "On" Characteristic here.  We are now going to replace these two lines (by commenting them out)...
 
-  new SpanAccessory();
-    new Service::AccessoryInformation();
-      new Characteristic::Identify();
-    new LED(15); 
+  //  new Service::LightBulb();                   
+  //    new Characteristic::On();                 
 
-   
-  // Accessory 2: Relay Switch
-  new SpanAccessory();
+  // ...with a single new line instantiating a new class we will call DEV_LED():
+
+    new DEV_LED(16);        // this instantiates a new LED Service.  Where is this defined?  What happpened to Characteristic::On?  Keep reading...
+
+  // The full definition and code for DEV_LED is implemented in a separate file called "DEV_LED.h" that is specified using the #include at the top of this program.
+  // The prefix DEV_ is not required but it's a helpful convention when naming all your device-specific Services. Note that DEV_LED will include all the required
+  // Characterictics of the Service, so you DO NOT have to separately instantiate Characteristic::On --- everything HomeSpan needs for DEV_LED should be implemented
+  // in DEV_LED itself (though it's not all that much).  Finally, note that we created DEV_LED to take a single integer argument.  If you guessed this is
+  // the number of the Pin to which you have attached an LED, you'd be right.  See DEV_LED.h for a complete explanation of how it works.
+
+  new SpanAccessory(); 
+  
     new Service::AccessoryInformation();    
       new Characteristic::Identify();                       
-    new DEV_RELAY(17);  // instantiates a new relay
-}
 
-void loop() {
+  //  new Service::LightBulb();                       // Same as above, this line is deleted...
+  //    new Characteristic::On();                     // This line is also deleted...
+  
+    new DEV_LED(17);                                  // ...and replaced with a single line that instantiates a second DEV_LED Service on Pin 17
+
+} // end of setup()
+
+//////////////////////////////////////
+
+void loop(){
   
   homeSpan.poll();
-
-}
+  
+} // end of loop()
